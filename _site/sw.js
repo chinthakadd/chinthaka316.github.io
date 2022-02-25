8531e8475f63322be6fc2c0459dfafa51247603e
Chinthaka Dharmasiri <chinthakadd@gmail.com>
2022-02-25 02:09:14 -0500

    push again

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220225.0705';
+const cacheName = 'chirpy-20220225.0709';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220225.0525';
+const cacheName = 'chirpy-20220225.0708';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
